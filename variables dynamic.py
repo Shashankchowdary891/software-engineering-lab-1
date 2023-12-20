657821d8bd96e7602fae8b189c38670a5d1193c2
Shashankchowdary891 <153701813+Shashankchowdary891@users.noreply.github.com>
2023-12-20 09:39:57 +0530

    variables dynamic.py

---
+++ variables dynamic.py
@@ -0,0 +1,26 @@
+def weather-Condition (T,H,S) : W=(0.5^{}(7^{}_{2}))-(0.2^{}+1)+(0.1^{*}w)-15 
+Print("Weather:", w)
+
+if~w>300
+
+Print ("Prediction: Sunny")
+
+elif 200<W<=300;
+
+Print ("Prediction; cloudy")
+
+elif 100 <W<=200
+
+Print ("Prediction: Rainy")
+
+elif w<=\bullet100
+
+Print ("Prediction: Stromy")
+
+T₁ = int int (input("Enter Temperature "))
+
+T H₁ = int(input("Enter Humidity: "))"
+
+w1=int (input("Enter wind: "))
+
+Weather Condition (\tau_{1},H_{1},\omega_{1})
